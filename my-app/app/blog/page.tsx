bd8bfb3c9d7fbda581a71c29cfcc0f34568230f3
mohNurCode <mamehashim00@gmail.com>
2026-01-19 08:44:41 -0800

    first nextjs nested routs

---
+++ my-app/app/blog/page.tsx
@@ -0,0 +1,5 @@
+export default function Blog(){
+    return (
+        <h1>my first blog</h1>
+    )
+}
---
+++ my-app/app/blog/second/page.tsx
@@ -0,0 +1,5 @@
+export default function second(){
+    return(
+        <h1>second blog</h1>
+    )
+}
